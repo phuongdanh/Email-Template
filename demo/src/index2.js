e3c11be1357a2e460e8aadab8efc41855cf2383e
tp danh <danh.tp@terra-drone.co.jp>
2023-01-18 11:22:20 +0700

    update

---
+++ demo/src/index2.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import styled, { createGlobalStyle } from 'styled-components';
+
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import Example from './example';
+import Dashboard from './dashboard';
+
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+  }
+
+  #demo {
+    height: 100%;
+  }
+`;
+
+class Demo extends Component {
+  render() {
+    return (
+      <Example />
+    );
+  }
+}
+
+ReactDOM.render(<Demo />, document.querySelector('#demo'));
